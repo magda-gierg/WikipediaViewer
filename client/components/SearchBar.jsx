75927eb4be17eaea85c9bd2b300ee1fcdaa2cc0e
magda-gierg <mjgiergiczny@gmail.com>
2018-06-05 09:20:04 +1200

    SearchBar handleInputChange

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
-    render() {
-        return (
-            <div className="search-box-container" >
-                <form>
-                    <input className="search-box-text" type="text" placeholder="Search for something..."/>
-                </form>
-                <p className="random-text"><a href="http://en.wikipedia.org/wiki/Special:Random" target="_blank">random article</a></p>
-            </div>
-        );
+  constructor() {
+    super()
+    this.state = {
+      serchTerm: ''
     }
+  }
+  handleInputChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div className="search-bar-container" >
+        <form>
+          <input className="search-bar-text" type="text" placeholder="Search for something" onChange={this.handleInputChange.bind(this)} value={this.state.searchTerm}/>
+        </form>
+        <p className="random-text"><a href="http://en.wikipedia.org/wiki/Special:Random" target="_blank">random article</a></p>
+      </div>
+    );
+  }
 }
--- client/components/WikipediaViewer.jsx
+++ client/components/WikipediaViewer.jsx
@@ -8,9 +8,7 @@ export default class WikipediaViewer extends React.Component {
                 <input type="text" placeholder="Search for something!"/>
                 <ul>
                     <li>Result 1</li>
-                    <li>Result 2</li>
-                    <li>Result 3</li>
-                    <li>...</li>
+                    
                 </ul>
             </div>
         );
